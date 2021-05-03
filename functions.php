3818e259780e208eb8274e0c8f5ef2948d57763b
Roman <utekun@gmail.com>
2021-05-03 15:45:58 +0300

    support of `Myriad Pro` font family

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
 function reobiz_child_enqueue_scripts()
 {
 	wp_enqueue_style( 'reobiz-child', get_template_directory_uri() . '/style.css' );
+
+	wp_enqueue_style( 'font-myriad-pro', get_stylesheet_directory_uri() . '/fonts/myriad-pro.css' );
 }
 
 add_action('wp_enqueue_scripts', 'reobiz_child_enqueue_scripts', 11);
